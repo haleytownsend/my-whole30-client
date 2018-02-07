8b90a4e552c559d645c216c277531ddaed926206
Haley Townsend <haleyrk@hotmail.com>
2018-02-07 14:58:55 -0600

    routes

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -11,6 +11,11 @@ router.get('/', (req, res) => {
 
 
 //GET by ID
+router.get('/:id', (req, res) => {
+  User.findById(req.params['id']).exec()
+  .then(user => {res.status(200).json(user).end())
+  .catch(message => {res.status(404).json({ error: true, message }).end()})
+})
 
 
 //POST
@@ -21,10 +26,28 @@ router.post('/', (req, res) => {
         throw new Error ('\'${field}\' is not in request body')
       }
     }))
+    .then (() => User.create({
+      email: req.body.email,
+      password: req.body.password
+    }))
+    .then(user => res.status(201).json(user).end())
+    .catch(message => res.status(400).json({ error: true, message }).end())
 })
 
 
 //PUT
+router.put('/:id', (req, res) => {
+  return Promise.resolve()
+    .then() => User.validate(req.body))
+    .then() => User.findByIdAndUpdate(req.body.id, req.body))
+    .then(user => res.status(200).json(user).end()
+    .catch(user => res.status(500).json({ error: true, message }).end())
+})
 
 
 //DELETE
+ router.delete('/:id', (req, res) => {
+   User.findByIdAndRemove(req.params.id).exec()
+    .then(user => res.status(200).end())
+    .catch(message => res.status(404).json({ error: true, message }).end())
+ })
